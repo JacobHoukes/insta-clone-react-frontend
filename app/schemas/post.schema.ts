e7ff31652479a8b530111bcdae48b12cc50340ad
Jacob <jayhoukes@gmail.com>
2025-12-06 09:01:02 +0100

    updated post.schema.ts, incl. file type validation

--- app/schemas/post.schema.ts
+++ app/schemas/post.schema.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const postSchema = z.object({
   id: z.number(),
@@ -14,3 +14,25 @@ type Post = z.infer<typeof postSchema>;
 export { postSchema, postsSchema };
 export type { Post };
 
+// Schema for creating a new post (for frontend validation)
+export const createPostInputSchema = z
+  .object({
+    caption: z.string().min(1, 'Caption is required.').max(255).optional(),
+    image: z
+      .instanceof(File)
+      .optional() // For file input
+      .refine(
+        (file) => {
+          if (!file) return true;
+          const valid_types = ['image/jpeg', 'image/png', 'image/gif'];
+          return valid_types.includes(file.type);
+        },
+        { message: 'Invalid file type.' }
+      ),
+  })
+  .refine((data) => data.caption || data.image, {
+    message: 'Either an image or a caption is required.',
+    path: ['image'], // Attach error to image field if both are missing
+  });
+
+export type CreatePostInput = z.infer<typeof createPostInputSchema>;
